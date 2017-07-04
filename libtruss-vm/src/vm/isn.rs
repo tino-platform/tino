1a286eaf16a2895ec19db75782509c6f315786e5
treyzania <treyzania@gmail.com>
2017-07-03 23:14:50 -0400

    Added instructions for logical operations.

--- libtruss-vm/src/vm/isn.rs
+++ libtruss-vm/src/vm/isn.rs
@@ -1,5 +1,6 @@
 use process::Process;
 use process::heap::StackValue;
+use process::heap::HeapValue::*;
 
 #[derive(Clone)]
 pub enum Instruction {
@@ -7,7 +8,13 @@ pub enum Instruction {
     PushLiteral(StackValue),
     Pop,
     Dup,
-    Xchg
+    Xchg,
+    Add,
+    Mul,
+    LogicalAnd,
+    LogicalOr,
+    LogicalXor,
+    LogicalNot
 }
 
 impl Instruction {
@@ -57,6 +64,201 @@ impl Instruction {
 
                 } else {
                     Err("not enough elements on stack")
+                }
+
+            },
+
+            &Instruction::Add => {
+
+                let a = p.stack.pop();
+                let b = p.stack.pop();
+
+                match (a, b) {
+                    (Some(x), Some(y)) => {
+
+                        match (x, y) {
+
+                            (Integer(x), Integer(y)) => {
+                                p.stack.push(Integer(x + y));
+                                Ok(())
+                            },
+
+                            (Integer(x), Byte(y)) => {
+                                p.stack.push(Integer(x + y as i64));
+                                Ok(())
+                            },
+
+                            (Byte(x), Integer(y)) => {
+                                p.stack.push(Integer(x as i64 + y));
+                                Ok(())
+                            },
+
+                            (Byte(x), Byte(y)) => {
+                                p.stack.push(Byte(x + y));
+                                Ok(())
+                            },
+
+                            _ => Err("incompatible stack item types")
+
+                        }
+
+                    },
+                    (Some(x), None) => {
+                        p.stack.push(x);
+                        Err("stack empty")
+                    },
+                    _ => Err("stack empty")
+                }
+
+            }
+
+            &Instruction::Mul => {
+
+                let a = p.stack.pop();
+                let b = p.stack.pop();
+
+                match (a, b) {
+                    (Some(x), Some(y)) => {
+
+                        match (x, y) {
+
+                            (Integer(x), Integer(y)) => {
+                                p.stack.push(Integer(x * y));
+                                Ok(())
+                            },
+
+                            (Integer(x), Byte(y)) => {
+                                p.stack.push(Integer(x * y as i64));
+                                Ok(())
+                            },
+
+                            (Byte(x), Integer(y)) => {
+                                p.stack.push(Integer(x as i64 * y));
+                                Ok(())
+                            },
+
+                            (Byte(x), Byte(y)) => {
+                                p.stack.push(Byte(x * y));
+                                Ok(())
+                            },
+
+                            _ => Err("incompatible stack item types")
+
+                        }
+
+                    },
+                    (Some(x), None) => {
+                        p.stack.push(x);
+                        Err("stack empty")
+                    },
+                    _ => Err("stack empty")
+                }
+
+            }
+
+            &Instruction::LogicalAnd => {
+
+                let a = p.stack.pop();
+                let b = p.stack.pop();
+
+                match (a, b) {
+                    (Some(x), Some(y)) => {
+
+                        match (x, y) {
+
+                            (Boolean(x), Boolean(y)) => {
+                                p.stack.push(Boolean(x && y));
+                                Ok(())
+                            },
+
+                            _ => Err("incompatible stack item types")
+
+                        }
+
+                    },
+                    (Some(x), None) => {
+                        p.stack.push(x);
+                        Err("stack empty")
+                    },
+                    _ => Err("stack empty")
+                }
+
+            }
+
+            &Instruction::LogicalOr => {
+
+                let a = p.stack.pop();
+                let b = p.stack.pop();
+
+                match (a, b) {
+                    (Some(x), Some(y)) => {
+
+                        match (x, y) {
+
+                            (Boolean(x), Boolean(y)) => {
+                                p.stack.push(Boolean(x || y));
+                                Ok(())
+                            },
+
+                            _ => Err("incompatible stack item types")
+
+                        }
+
+                    },
+                    (Some(x), None) => {
+                        p.stack.push(x);
+                        Err("stack empty")
+                    },
+                    _ => Err("stack empty")
+                }
+
+            }
+
+            &Instruction::LogicalXor => {
+
+                let a = p.stack.pop();
+                let b = p.stack.pop();
+
+                match (a, b) {
+                    (Some(x), Some(y)) => {
+
+                        match (x, y) {
+
+                            (Boolean(x), Boolean(y)) => {
+                                p.stack.push(Boolean(x ^ y));
+                                Ok(())
+                            },
+
+                            _ => Err("incompatible stack item types")
+
+                        }
+
+                    },
+                    (Some(x), None) => {
+                        p.stack.push(x);
+                        Err("stack empty")
+                    },
+                    _ => Err("stack empty")
+                }
+
+            }
+
+            &Instruction::LogicalNot => {
+
+                let val = p.stack.pop();
+
+                match val {
+                    Some(v) => match v {
+                        Boolean(x) => {
+                            p.stack.push(Boolean(!x));
+                            Ok(())
+                        },
+                        _ => {
+                            p.stack.push(v);
+                            Err("not a boolean")
+                        }
+                    },
+                    _ => Err("stack empty")
                 }
 
             }
